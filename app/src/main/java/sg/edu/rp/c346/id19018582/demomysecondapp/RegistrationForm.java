d4ed95f6f799472b2553532daf56058b880a5f92
LU JING <19018582@myrp.edu.sg>
2020-04-30 19:04:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19018582/demomysecondapp/RegistrationForm.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19018582.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+    }
+}
